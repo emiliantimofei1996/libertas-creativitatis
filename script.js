1e631919b1f63fcb4d6142121743bdb6dfe62090
emiliantimofei1996 <emilian.timofei@hey.com>
2025-11-10 16:59:03 +0100

    searching for a backtick to use js

---
+++ script.js
@@ -0,0 +1,9 @@
+fromOneToFive = ""
+
+
+for (let i = 0; i < 5; i++) {
+    let elemento = document.getSelectorAll("main>ul>li")
+    let colore = getComputedStyle(document.documentElement).getPropertyPriority()
+    console.log(i)
+}
+
