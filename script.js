a941d6e87e4e817b20b8cef8cbb7b453b3ea97ef
emiliantimofei1996 <emilian.timofei@hey.com>
2025-11-10 18:00:13 +0100

    done

--- script.js
+++
@@ -1,9 +0,0 @@
-fromOneToFive = ""
-
-
-for (let i = 0; i < 5; i++) {
-    let elemento = document.getSelectorAll("main>ul>li")
-    let colore = getComputedStyle(document.documentElement).getPropertyPriority()
-    console.log(i)
-}
-
